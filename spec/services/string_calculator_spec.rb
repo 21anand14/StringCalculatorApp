d5e113a06262669fd6b97a33e077026ed0829180
Anand <21anand14@gmail.com>
2025-04-12 13:34:18 +0530

    handling newlines between numbers

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -17,5 +17,13 @@ RSpec.describe StringCalculator do
     it 'returns the sum of multiple comma-separated numbers' do
       expect(described_class.new.add("1,2,3,4,5")).to eq(15)
     end
+
+    it 'handles newlines between numbers' do
+      expect(described_class.new.add("2\n2,3")).to eq(7)
+    end
+
+    it 'handles multiple newlines between numbers' do
+      expect(described_class.new.add("2\n2\n3")).to eq(7)
+    end
   end
 end
